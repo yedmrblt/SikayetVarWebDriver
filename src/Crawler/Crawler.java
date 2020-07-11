6752fe2c0a8245bf1da7f359b4990a35f9258046
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2020-07-11 11:46:07 +0300

    complaints are written to file

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -1,5 +1,8 @@
 package Crawler;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -12,18 +15,28 @@ import org.openqa.selenium.support.ui.Sleeper;
 
 public class Crawler {
 	WebDriver driver;
+	FileWriter fw;
+	File file = new File("complains.txt");
 	
 	public void getMessages() {
-		List<WebElement> listOfElements = driver.findElements(By.xpath("//h5"));
-		for (WebElement webElement: listOfElements) {
-			System.out.println(webElement.getText());
+		try {
+			fw = new FileWriter(file, true);
+			List<WebElement> listOfElements = driver.findElements(By.xpath("//h5"));
+			for (WebElement webElement: listOfElements) {
+				fw.write(webElement.getText());
+				fw.write(System.getProperty("line.separator"));
+			}
+			WebElement nextPageArrow = driver.findElement(By.xpath("//a[contains(@class, 'right-arrow')]"));
+			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+			nextPageArrow.click();
+			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+			fw.flush();
+			fw.close();
+			getMessages();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println("-------------------------------");
-		WebElement nextPageArrow = driver.findElement(By.xpath("//a[contains(@class, 'right-arrow')]"));
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-		nextPageArrow.click();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		getMessages();
 	}
 	
 	public void launchBrowser() {
@@ -37,11 +50,13 @@ public class Crawler {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
 		driver = new ChromeDriver();
 		
-		driver.get("https://www.sikayetvar.com/mng-kargo");
+		driver.get("https://www.sikayetvar.com/mng-kargo?page=1");
 		// cerez politikasi
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//button[contains(@class, 'btn-complain') and contains(., 'Kabul')]")).click();
+		
 		getMessages();
+		
 	}
 	
 	public static void main(String[] args) {
